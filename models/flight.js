37d8395faebf3a190022804cc62dd8fffb62adee
gizeli-pereira <gizeli.pereira1@gmail.com>
2023-03-31 09:28:27 -0500

    Add mongoose, add partials and model

---
+++ models/flight.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+//shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: String,
+    airport: String,
+    flightNo: Number,
+    departs: Date
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
